0f260fc1ad9c2e4a69da290f712088f947740b6c
Savva <sav.van.ivan3@mail.ru>
2026-02-27 00:41:20 +0300

    Initial commit

---
+++ src/test/java/com/innowise/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
